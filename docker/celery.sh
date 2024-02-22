76d5403eac0e84b2ecc7f17a8b83284c1bfc5977
telega1985 <telega.85@gmail.com>
2024-02-22 16:08:20 +0200

    Добавил flower.sh

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ "${1}" == "celery" ]]; then
-  celery --app=app.tasks.celery_app:celery worker -l INFO
-elif [[ "${1}" == "flower" ]]; then
-  celery --app=app.tasks.celery_app:celery flower
-fi
+celery --app=app.tasks.celery_app:celery worker -l INFO
