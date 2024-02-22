d9cbed23ba2ce7c676ffc687098f57d2dcb5422c
telega1985 <telega.85@gmail.com>
2024-02-22 17:32:44 +0200

    Убрал periodic tasks

--- app/tasks/celery_app.py
+++ app/tasks/celery_app.py
@@ -1,5 +1,4 @@
 from celery import Celery
-from celery.schedules import crontab
 
 from app.config import settings
 
@@ -7,14 +6,5 @@ from app.config import settings
 celery = Celery(
     "tasks",
     broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
-    include=["app.tasks.tasks", "app.tasks.scheduled"]
+    include=["app.tasks.tasks"]
 )
-
-
-celery.conf.beat_schedule = {
-    "luboe-nazvanie": {
-        "task": "periodic_task",
-        "schedule": 5, # секунды
-        # "schedule": crontab(minute="30", hour="15")
-    }
-}
--- app/tasks/scheduled.py
+++ app/tasks/scheduled.py
@@ -1,6 +1,1 @@
-from app.tasks.celery_app import celery
 
-
-@celery.task(name="periodic_task")
-def periodic_task():
-    print(12345)
